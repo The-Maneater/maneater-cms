921e5a36b143f5c422350dbd8d976655c1935acf
Michael Smith <mdsf3f@mail.missouri.edu>
2017-11-12 01:40:11 -0600

    Possible tag fix

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Repositories\AdRepository;
 use App\Story;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Spatie\Tags\Tag;
 
 class TagsController extends Controller
@@ -13,9 +14,17 @@ class TagsController extends Controller
     {
         $tag = Tag::where('slug->en', $slug)
             ->first();
-        $stories = Story::withAllTags([$tag->name])
-            ->orderBy('id', 'DESC')
-            ->paginate(20);
+        $page = 0;
+        if(request()->has('page')) $page = request('page') - 1;
+        $itemStories = Story::withAllTags([$tag->name])->orderBy('id', 'DESC')->offset($page * 20)->take(20)->get()->load('headerPhotos');
+        $count = Story::withCount(['tags' => function($query) use ($tag) {
+            $query->where('tag_id', $tag->id);
+        }])->first()->tags_count;
+        $stories = new LengthAwarePaginator($itemStories, $count, 20, $page);
+
+//        $stories = Story::withAllTags([$tag->name])
+//            ->orderBy('id', 'DESC')
+//            ->paginate(20);
         $ads = AdRepository::cubes(2);
 
         return view('tags.show', compact('tag', 'stories', 'ads'));
