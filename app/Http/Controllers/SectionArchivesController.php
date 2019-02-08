23c9d6dfdc51161b37a6085e9fa04fb501cd0c8b
Joshua Thompson <jthompson706.jt@gmail.com>
2019-02-08 14:13:01 -0600

    update section archives view

--- app/Http/Controllers/SectionArchivesController.php
+++ app/Http/Controllers/SectionArchivesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Paginators\PublishDatePaginator;
 use App\Section;
+use App\Story;
+use App\Repositories\AdRepository;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 
@@ -11,19 +13,22 @@ class SectionArchivesController extends Controller
 {
     public function show($slug)
     {
-        $page = 0;
-        if(request()->has('page')) $page = request('page') - 1;
+
+
+        // get subsection object
         $section = Section::findBySlug($slug);
+
+        // get subsection name
         $sectionName = $section->name;
-        $count = $section->withCount('stories')->get()->first(function ($inner) use ($section) {
-            return $inner->slug === $section->slug;
-        })->stories_count;
-        $stories = $section->stories()->latest('publish_date')->offset($page * 20)->take(20)->get()->load('headerPhotos');
-        $paginator = (new PublishDatePaginator($stories, $count))->paginate(20, $page)->withPath("/section/{$section->slug}/archives");
-//        $paginatedStories = collect(array_slice($stories->toArray(), $this->perPage * $page, $this->perPage, true))
-//        ->groupBy('publish_date');
-//        $paginator = new LengthAwarePaginator($paginatedStories, count($stories), $this->perPage, $page);
-        //dd($paginator);
-        return view('sections.archives', compact('paginator', 'sectionName'));
+
+        // get stories and paginate them
+        $stories = Story::where('section_id', $section->id)->paginate(9);
+
+        // get ads
+        $ads = AdRepository::cubes(2);
+
+        // pass variables to archives page
+        return view('sections.archives', compact('stories', 'sectionName', 'ads'));
+
     }
 }
